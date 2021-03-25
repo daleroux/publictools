14b942dca2a1dd96142fa22f4cd746f2a9b47977
Daniel Leroux <d@nleroux.ca>
2021-03-25 13:06:39 -0400

    generalized the restart

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Login into awx tower
-export TOWER_HOST=https://192.168.0.23
-export TOWER_OAUTH_TOKEN=LOI4raOPlpRYVuT6xCIpsCYjBArr49
+export TOWER_HOST=https://<IP ADDRESS OF FQDN HERE>
+export TOWER_OAUTH_TOKEN=<OAUTH TOKEN HERE>
 
 # Creating the Organization
 awx -k job_templates launch restart_splunk  --monitor -f human
